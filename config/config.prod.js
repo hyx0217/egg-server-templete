21316b58e8192f3490469383b2367647fc00e74f
Stone <1014959288@qq.com>
2020-05-15 23:35:21 +0800

    update：服务器配置

--- config/config.prod.js
+++ config/config.prod.js
@@ -10,7 +10,9 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {
+    baseUrl: 'http://106.15.121.64/server',
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1586587199716_572';
