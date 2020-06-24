88bd1f5d1803686d2454149e2447d28975f4adda
Stone <1014959288@qq.com>
2020-06-24 16:51:18 +0800

    add:新增alinode配置

--- config/config.default.js
+++ config/config.default.js
@@ -11,6 +11,18 @@ module.exports = appInfo => {
    * @type {Egg.EggAppConfig}
    **/
   const config = exports = {};
+  config.alinode = {
+    server: 'wss://agentserver.node.aliyun.com:8080',
+    appid: '85354',
+    secret: '7b153115ce7880388e90e80ce4ffc3dcebafb8d0',
+    logdir: '/tmp/',
+    /*     error_log: [
+      '您的应用在业务层面产生的异常日志的路径，数组，可选，可配置多个',
+      '例如：/root/.logs/error.#YYYY#-#MM#-#DD#.log',
+      '不更改 Egg 默认日志输出路径可不配置本项目',
+    ], */
+    /*    agentidMode:'IP' '可选，如果设置，则在实例ID中添加部分IP信息，用于多个实例 hostname 相同的场景（以容器为主）' */
+  };
   config.security = {
     csrf: {
       headerName: 'token',
