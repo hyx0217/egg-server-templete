b8e4352b368e93a594f390bccd8dc91402fdee42
Stone <1014959288@qq.com>
2020-06-24 16:57:45 +0800

    update

--- config/plugin.js
+++ config/plugin.js
@@ -3,6 +3,10 @@
 /** @type Egg.EggPlugin */
 module.exports = {
   // had enabled by egg
+  alinode: {
+    enable: true,
+    package: 'egg-alinode',
+  },
   cors: {
     enable: true,
     package: 'egg-cors',
